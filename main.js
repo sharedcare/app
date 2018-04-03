161f9273561fb20250a7a27ef801c9c8e22f264f
sharedcare <sharedcare@hotmail.com>
2018-04-02 22:39:18 -0500

    Add sidebar

--- main.js
+++ main.js
@@ -11,10 +11,16 @@ $(document).ready(function() {
         })
     ;
 
+    // Menu
+    $('.menu .menu>a').click(function () {
+        $(this).addClass('active');
+        $(this).siblings().removeClass('active');
+    });
+
     // Pop up
     $('.menu .popup.menu>a').click(function () {
-        $(this).addClass('teal active');
-        $(this).siblings().removeClass('teal active');
+        $(this).addClass('teal');
+        $(this).siblings().removeClass('teal');
     });
 
     $('.item.site').popup({
@@ -26,6 +32,17 @@ $(document).ready(function() {
             hide: 500
         }
     });
+
+    // Sidebar
+    $('.ui.sidebar')
+        .sidebar('setting', 'transition', 'overlay')
+        .sidebar('attach events', '.menu .ui.browse.item')
+    ;
+
+    // Accordion
+    $('.ui.accordion')
+        .accordion()
+    ;
 
     search();
 });
@@ -45,7 +62,70 @@ function search() {
         }
     ];
 
-    $('.ui.search').search({
+    var siteContent = [
+        {
+            title: 'Home',
+            url: '//sharedcare.io',
+            description: 'Homepage of sharedcare.io',
+        },
+        {
+            title: 'Profile',
+            url: '//sharedcare.io/profile/',
+            description: 'Profile page',
+        },
+        {
+            title: 'Gallery',
+            url: '//sharedcare.io/gallery/',
+            description: 'Gallery page',
+        },
+        {
+            title: 'Log',
+            url: '//sharedcare.io/log/',
+            description: 'Log page',
+        },
+        {
+            title: 'Resume',
+            url: '//sharedcare.io/pdf/Resume.pdf',
+            description: 'My resume pdf file',
+        },
+        {
+            title: 'Sitemap',
+            url: '//sharedcare.io/sitemap.html',
+            description: 'Site Map page for user',
+        },
+        {
+            title: 'Sitemap.xml',
+            url: '//sharedcare.io/sitemap.xml',
+            description: 'XML site map for search bot',
+        },
+        {
+            title: 'Home - Image Captioning',
+            url: '//sharedcare.io/ImageCaptioning/',
+            description: 'A deep learning project',
+        },
+        {
+            title: 'Proposal - Image Captioning',
+            url: '//sharedcare.io/ImageCaptioning/proposal/',
+            description: 'The proposal of Image Captioning',
+        },
+        {
+            title: 'SharedApp',
+            url: '//app.sharedcare.io',
+            description: 'Homepage of SharedApp',
+        },
+        {
+            title: 'Grader | SharedApp',
+            url: '//app.sharedcare.io/grader/',
+            description: 'An efficient excel editor',
+        },
+        {
+            title: 'Blog',
+            url: '//blog.sharedcare.io',
+            description: 'Homepage of blog',
+        }
+    ];
+
+    $('.ui.search.app').search({
         source : appContent,
         searchFields   : [
             'title',
@@ -54,4 +134,15 @@ function search() {
         ],
         fullTextSearch: 'exact'
     });
+
+    $('.ui.search.site').search({
+        source : siteContent,
+        searchFields   : [
+            'title',
+            'description',
+            'url'
+        ],
+        fullTextSearch: 'exact'
+    });
+
 }
